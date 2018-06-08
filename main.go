713cf99a59abd778144a27ae80074ff749a1cad8
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-06-08 17:17:40 -0400

    Add load and save image functions

--- main.go
+++ main.go
@@ -2,7 +2,34 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
 )
+
+func loadImage(filename string) image.Image {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("os.Open failed: %v", err)
+	}
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatalf("image.Decode failed: %v", err)
+	}
+	return img
+}
+
+func saveImage(filename string, img image.Image) {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("os.Create failed %v", err)
+	}
+	err = png.Encode(f, img)
+	if err != nil {
+		log.Fatalf("png.Encode failed %v", err)
+	}
+}
 
 func main() {
 	fmt.Print("Welcome to the Deep Fryer")
