d8c9390c184da1202e0a0ea0ee14637af28fd46b
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-06-19 00:02:08 -0400

    Add random position genration code. Add for loop to loop over the randomly picked emojis and attach them to the source image at the position

--- emoji/emoji.go
+++ emoji/emoji.go
@@ -14,6 +14,10 @@ import (
 	"github.com/disintegration/imaging"
 )
 
+type pos struct {
+	x, y int
+}
+
 // LoadImage takes in a filename, reads the image if any from the location and returns it
 func LoadImage(filename string) image.Image {
 	f, err := os.Open(filename)
@@ -50,9 +54,42 @@ func getAssetNames(assetDir string) []string {
 	return filenames
 }
 
-// func loadRandomAssets(assetNames []string, numToLoad int) []image.Image {
+func loadRandomAssets(assetNames []string, numToLoad int) []image.Image {
+	rand.Seed(time.Now().Unix())
+	randAssets := []image.Image{}
+	set := make(map[int]struct{})
 
-// }
+	for {
+		randValue := rand.Intn(numToLoad)
+		if _, ok := set[randValue]; ok {
+			continue
+		} else {
+			set[randValue] = struct{}{}
+		}
+		if len(set) == numToLoad {
+			break
+		}
+	}
+
+	for i := range set {
+		randAssets = append(randAssets, LoadImage(assetNames[i]))
+	}
+
+	return randAssets
+}
+
+func genRandomEmojiPositions(emjBnds image.Rectangle, srcBnds image.Rectangle) image.Pt {
+	// randPos := []pos{}
+	DX := srcBnds.Dx()
+	DY := srcBnds.Dy()
+
+	dx := emjBnds.Dx()
+	dy := emjBnds.Dy()
+	rand.Seed(time.Now().Unix())
+	randX := rand.Intn(DX-dx) + dx/2
+	randY := rand.Intn(DY-dy) + dy/2
+	return image.Pt(randX, randY)
+}
 
 // AddEmojis is a function that will add emojis randomly to the given
 // image
@@ -62,9 +99,15 @@ func AddEmojis(src image.Image) *image.RGBA {
 	emoImg := image.NewRGBA(src.Bounds())
 	gift.New().Draw(emoImg, src)
 
-	// assets := getAssetNames("../assets/")
+	assets := getAssetNames("../assets/")
+	numAssetsToUse := rand.Intn(len(assets))
 
-	// numAssetsToUse := rand.Intn(len(assets))
+	randAssets := loadRandomAssets(assets, numAssetsToUse)
+
+	for i := randAssets {
+		randPos := genRandomEmojiPositions(randAssets[i].Bounds, src.Bounds)
+		gift.New().DrawAt(emoImg, randAssets[i], randPos, gift.OverOperator)
+	}
 
 	return emoImg
 }
